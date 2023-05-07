b2cda9564a87e6fb6dc2b0b54f9d0d1121a58a61
Oliver Hernandez <116779111+Meeseri@users.noreply.github.com>
2023-05-07 16:04:14 +0000

    Main

--- Examples/Webber/CalcJS.js
+++ Examples/Webber/CalcJS.js
@@ -1,64 +1,64 @@
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.clear()
-    }
+        constructor(previousOperandTextElement, currentOperandTextElement) {
+            this.previousOperandTextElement = previousOperandTextElement
+            this.currentOperandTextElement = currentOperandTextElement
+            this.clear()
+        }
 
 
     clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
   }
 
     delete() {
-    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
 
     apprendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) return
-    this.currentOperand = this.currentOperand.toString() + number.toString()
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
 
 }
 
 
     chooseOperation(operation) {
-    if (this.currentOperand === '') return 
-    if (this.previousOperand !== ''){
+        if (this.currentOperand === '') return 
+        if (this.previousOperand !== ''){
         this.compute()
-    }
-    this.operation = operation
-    this.previousOperand = this.currentOperand
-    this.currentOperand = ''
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
 }
 
 
     compute() {
-    let computation
-    const prev = parseFloat(this.previousOperand)
-    const current = parseFloat(this.currentOperand)
-    if (isNaN(prev) || isNaN(current)) return
-    switch (this.operation) {
-      case '+':
-        computation = prev + current
-        break
-      case '-':
-        computation = prev - current
-        break
-      case '*':
-        computation = prev * current
-        break
-      case '÷':
-        computation = prev / current
-        break
-      default:
-        return
-    }
-    this.currentOperand = computation
-    this.operation = undefined
-    this.previousOperand = ''
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
   }
 
 
